fee7297cbea8a890d06a18047988815021b07042
Jose A <jvazquez@bnl.gov>
2020-04-14 17:42:25 -0500

    Compress pantheo on 20 bins

--- Cosmo/ParamDefs.py
+++ Cosmo/ParamDefs.py
@@ -93,6 +93,8 @@ b_par = Parameter("b", 0., 0.5, (-10., 10.), "b")
 
 
 #Compress data
-zbin_par = [Parameter("zbin%d"%i, 1.2*i,  0.1, (0.+i, 10.+i), "zbin%d"%i) for i in range(1, 10)]
+Nbins = 20
+step  =  (13-1.3)/(Nbins-1)
+zbin_par = [Parameter("zbin%d"%i, 1.3+step*i, 0.2, (1+i, 3+step*i), "zbin%d"%i) for i in range(Nbins)]
 
 
