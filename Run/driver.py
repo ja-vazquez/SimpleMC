9ed3bc9dbec19ef832180e3b383141029f6b02df
igomezv <neorelativista@gmail.com>
2020-04-22 02:21:34 -0500

    ini path

--- Run/driver.py
+++ Run/driver.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from DriverMC import DriverMC
-#from PlotterMC import PlotterMC
 
-fileConfig = "/Users/josevazquezgonzalez/Desktop/Codigos_SimpleMC/SimpleMC/baseConfig.ini"
+from DriverMC import DriverMC
+
+fileConfig = "baseConfig.ini"
 
 D = DriverMC(fileConfig)
 
 #D.plotter()
+
