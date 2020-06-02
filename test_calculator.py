e0c7ce71aabe46609784cbc0bcccd1d49aeb2ea5
Jose A <jvazquez@bnl.gov>
2020-06-02 18:38:13 -0500

    cosmo calculator ready

---
+++ test_calculator.py
@@ -0,0 +1,20 @@
+from simplemc.CosmoCalc import CosmoCalc
+
+#Computes and plot (optional)
+#['HIOverrd', 'DVOverrd', 'DaOverrd', 'Hubble', 'HubInvOverz', 'SNIa', 'fs8']
+
+
+C = CosmoCalc('LCDM', 'Hubble')
+C.run_plot(lw='2')
+
+C = CosmoCalc('LCDM', 'Age')
+C.Age()
+
+#C = CosmoCalc('LCDM', 'DaOverrd', 'h', 0.4, 0.9)
+#C.run_plot(lw='2')
+
+
+#C = CosmoCalc('owaCDM', 'fs8', 'wa', -0.5, 0.5, 5, zmax=0.8)
+#C.run_plot(lw='1')
+
+
