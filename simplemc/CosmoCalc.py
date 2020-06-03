223c9fd4de001745a4716ea36f13fe87378368ab
Jose A <jvazquez@bnl.gov>
2020-06-02 22:03:02 -0500

    cosmo calcular with data

--- simplemc/CosmoCalc.py
+++ simplemc/CosmoCalc.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 
-
+from simplemc.tools.Data_plots import Data_plots
 from simplemc.runbase import ParseModel
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 import numpy as np
 import sys
 
@@ -13,28 +14,35 @@ class CosmoCalc:
     Cosmological calculator to plot the basic functions
     """
     def __init__(self, model, funct=None, param=None, minparam=None,
-                    maxparam=None, nsteps=4, savepdf=False, zmax=3):
+                    maxparam=None, nsteps=3, savepdf=False, zmax=3,
+                    plot_data=False):
         self.T     = ParseModel(model)
         self.model = model
         self.funct = funct
         self.param = param
+        self.Opar  = None
         if param:
             assert(maxparam > minparam)
             self.minparam = minparam
             self.maxparam = maxparam
             for i, par in enumerate(self.T.freeParameters()):
                 if self.param == par.name: self.Opar = par
-                else: continue
+            if self.Opar is None:
+                print('No param within model')
+                sys.exit(1)
 
-        self.nsteps  = nsteps
-        self.savepdf = savepdf
+        self.nsteps    = nsteps
+        self.savepdf   = savepdf
+        self.plot_data = plot_data
 
-        self.zl = np.linspace(0, zmax, 50)
+        self.plaw = 0.5
+        self.zmax = zmax
+        self.zl = np.linspace(0.05, self.zmax, 50)
 
         self.func_dic={'Hubble': self.Hubble, 'DaOverrd': self.DaOverrd,
                        'HIOverrd': self.HIOverrd, 'DVOverrd': self.DVOverrd,
-                       'HubInvOverz': self.HubInvOverz, 'SNIa': self.SNIa, 'fs8':self.fs8,
-                       'Age': self.Age}
+                       'HubInvOverz': self.HubInvOverz, 'SNIa': self.SNIa,
+                       'fs8':self.fs8, 'Age': self.Age}
         try:
             self.function  = self.func_dic[self.funct]
         except:
@@ -46,47 +54,82 @@ class CosmoCalc:
 
     def run_plot(self, **kwargs):
 
+        fig = plt.figure(figsize=(9, 6))
+        ax = fig.add_subplot(1, 1, 1)
+        ax.set_xscale('log')
+
+        if self.plot_data: self.selec_data(self.funct)
         if self.param is None:
-            y=[self.function(z) for z in self.zl]
-            self.plot_vars(y, ylabel=self.ylabel, **kwargs)
+            y= [self.function(z) for z in self.zl]
+            plt.plot(self.zl, y, **kwargs)
 
         else:
             for j in np.linspace(self.minparam, self.maxparam, self.nsteps):
                 self.Opar.setValue(j)
                 self.T.updateParams([self.Opar])
                 y= [self.function(z) for z in self.zl]
-                self.plot_vars(y, ylabel=self.ylabel,
-                               label='{}={:.2f}'.format(self.param, j), **kwargs)
+                label = '{}={:.2f}'.format(self.param, j)
+                plt.plot(self.zl, y, label=label, **kwargs)
+
+        plt.grid()
+        plt.title(self.model)
+        plt.xlim(0.05, self.zmax+0.1)
+        plt.xlabel("$z$", fontsize=25)
+        plt.ylabel(self.ylabel, fontsize=20)
+        plt.legend(loc='best', numpoints=1, frameon=False, fontsize=13)
+
+        # Axis
+        ax.xaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+        ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+
+        ax.xaxis.set_minor_formatter(plt.ScalarFormatter())
+        ax.xaxis.set_major_locator(plt.FixedLocator([0.1, 0.5]))
+        ax.xaxis.set_minor_locator(plt.FixedLocator([0.2, 1.0, 2]))
+        #plt.yticks(list(range(0, 50, 10)))
 
         if self.savepdf: plt.savefig('sm_{}_{}.pdf'.format(self.model, self.funct))
         plt.show()
 
 
+    def selec_data(self, name):
+        D = Data_plots()
+        if name == 'Hubble': D.Hubble_data()
+        if name == 'DaOverrd' : D.DaOverrd_data()
+        if name == 'DVOverrd' : D.DVOverrd_data()
+        if name == 'HIOverrd': D.HIOverrd_data()
+        if name == 'SNIa': D.SNIa_data()
+        if name == 'fs8': D.fs8_data()
+        else: pass
+
     #--- CC --
     def Hubble(self, z):
         function = 100*self.T.h*np.sqrt(self.T.RHSquared_a(1./(1+z)))
-        self.ylabel   = "$H(z)$"
+        self.ylabel   = r'$H(z) [km/s Mpc^{-1}]$'
+
         return function
 
 
     #--- BAO --
     def DaOverrd(self, z):
-        function = 1*self.T.DaOverrd(z)
-        self.ylabel = "$D_M(z)/r_d$"
+        function = 1*self.T.DaOverrd(z)/self.fixer(z)
+        self.ylabel = "$D_M(z)/r_d \sqrt{z}$"
+
         return function
 
 
     #--- BAO --
     def HIOverrd(self, z):
-        function = z*self.T.HIOverrd(z)
-        self.ylabel="$zD_H(z)$"
+        function = z*self.T.HIOverrd(z)/self.fixer(z)
+        self.ylabel="$zD_H(z)/r_d \sqrt{z}$"
+
         return function
 
 
     #--- BAO --
     def DVOverrd(self, z):
-        function = 1.*self.T.DVOverrd(z)
-        self.ylabel ="$D_v(z)/r_d$"
+        function = 1.*self.T.DVOverrd(z)/self.fixer(z)
+        self.ylabel ="$D_v(z)/r_d \sqrt{z}$"
+
         return function
 
 
@@ -98,15 +141,18 @@ class CosmoCalc:
 
     #--- SN --
     def SNIa(self, z):
-        function = self.T.distance_modulus(z) + 43
+        function = self.T.distance_modulus(z) + 23.8
         self.ylabel="$d_L(z)$"
+
         return function
+
 
 
     #--- fs8 --
     def fs8(self, z):
         function = 1.*self.T.fs8(z)
         self.ylabel="$f\sigma_8(z)$"
+
         return function
 
 
@@ -127,6 +173,11 @@ class CosmoCalc:
         plt.grid()
 
 
+    def fixer(self, z):
+        if self.plaw > 0:
+            return z**self.plaw
+        else:
+            return np.log(1.+z)
 
 
 if __name__ == "__main__":
--- test_calculator.py
+++ test_calculator.py
@@ -4,11 +4,11 @@ from simplemc.CosmoCalc import CosmoCalc
 #['HIOverrd', 'DVOverrd', 'DaOverrd', 'Hubble', 'HubInvOverz', 'SNIa', 'fs8']
 
 
-C = CosmoCalc('LCDM', 'Hubble')
-C.run_plot(lw='2')
+#C = CosmoCalc('LCDM', 'Hubble')
+#C.run_plot(lw='2')
 
-C = CosmoCalc('LCDM', 'Age')
-C.Age()
+#C = CosmoCalc('LCDM', 'Age')
+#C.Age()
 
 #C = CosmoCalc('LCDM', 'DaOverrd', 'h', 0.4, 0.9)
 #C.run_plot(lw='2')
@@ -17,4 +17,7 @@ C.Age()
 #C = CosmoCalc('owaCDM', 'fs8', 'wa', -0.5, 0.5, 5, zmax=0.8)
 #C.run_plot(lw='1')
 
+
+C = CosmoCalc('owaCDM', 'fs8', 'wa', -0.5, 0.5, 5, zmax=3.1, plot_data=True)
+C.run_plot(lw='1')
 
