30be4bec6a4356e2b95bad1177790b85e1298994
igomezv <neorelativista@gmail.com>
2020-06-03 05:11:22 -0500

    delete extra tests

--- neuralTest.py
+++
@@ -1,7 +0,0 @@
-from simplemc.DriverMC import DriverMC
-
-sampler = 'nested'
-a = DriverMC(analyzername=sampler ,model="LCDM", datasets="HD")
-a.executer(neuralNetwork=True)
-#a.nestedRunner(neuralNetwork=True)
-a.postprocess()
