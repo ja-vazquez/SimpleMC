17a800ae365d149d5c8f969861a5b8973deeaa8e
igomezv <neorelativista@gmail.com>
2020-06-14 15:23:26 -0500

    __init__ imports from base simplemc

--- simplemc/__init__.py
+++ simplemc/__init__.py
@@ -10,3 +10,16 @@ logger = logging.getLogger(__name__)
 if sys.version_info < (3, 0):
     logger.critical("You must use Python 3!")
     exit(1)
+
+from .PostProcessing import PostProcessing
+from .CosmoCalc import CosmoCalc
+from .runbase import ParseDataset, ParseModel
+from . import cosmo
+from . import analyzers
+from . import likelihoods
+from . import models
+from . import tools
+from . import attick
+
+
+__version__ = "1.9.0"
