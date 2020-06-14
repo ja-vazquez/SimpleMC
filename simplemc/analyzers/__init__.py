76574e40ab7a8c89123b348b744ae63a3e00590d
igomezv <neorelativista@gmail.com>
2020-06-14 15:09:33 -0500

    __init__ for analyzers

---
+++ simplemc/analyzers/__init__.py
@@ -0,0 +1,4 @@
+from .MaxLikeAnalyzer import MaxLikeAnalyzer
+from .MCMCAnalyzer import MCMCAnalyzer
+from .dynesty import DynamicNestedSampler, NestedSampler
+from .SimpleGenetic import SimpleGenetic
