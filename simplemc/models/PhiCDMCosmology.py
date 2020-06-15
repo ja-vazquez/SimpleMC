tiny mistake in a sign

simplemc/models/PhiCDMCosmology.py
@@ -7,7 +7,7 @@ from scipy.integrate import odeint
 from scipy.optimize import newton
 from simplemc.cosmo.paramDefs import phialp_par, philam_par, phimu_par, \
                       phibeta_par, Ok_par
-
+import sys
 
 class PhiCosmology(LCDMCosmology):
     def __init__(self, varyalpha=False, varybeta=False, varyilam=False,\
@@ -131,8 +131,8 @@ class PhiCosmology(LCDMCosmology):
 
         Ophi_prime  = -3*Ophi*(1+ wphi + 2*Pi/3.)
         Ok_prime    = -3*Ok*(1 - 1./3. + 2*Pi/3.)
-        wphi_prime  = -(1-wphi)*(3*(1+wphi)+self.eps*lam*term)
-        lam_prime   = self.eps*lam**2*(Mgamma -1)*term
+        wphi_prime  = -(1-wphi)*(3*(1+wphi) - self.eps*lam*term)
+        lam_prime   = -self.eps*lam**2*(Mgamma -1)*term
         hub_prime   = hub*Pi
 
         return [wphi_prime, Ophi_prime, lam_prime, Ok_prime, hub_prime]
@@ -166,7 +166,7 @@ class PhiCosmology(LCDMCosmology):
 
 
         #we'll use the sign of lambda to describe either quint or phant
-        #ini_lam=-ini_lam
+        ini_lam=-ini_lam
         self.eps = np.sign(ini_lam)
         ini_lam  =np.abs(ini_lam)
         self.ini_wphi = -1 + 1.0e-4*self.eps

simplemc/tools/plot_phi.py
@@ -113,7 +113,7 @@ for i in np.arange(min, max, step):
     elif model=='pow':      beta, mu, ilam = 0, i, 0.75
     elif model=='exp_pow2': beta, mu, alpha, ilam = i, 0, 2, 0.15
     elif model== 'pow_exp': beta, mu, alpha, ilam = -i, -1, 1, -0.75
-    elif model== 'exp_pow_a':beta, mu, alpha, ilam = 1.2, 0, i, 0.75
+    elif model== 'exp_pow_a':beta, mu, alpha, ilam = -1.2, 0, i, -0.45
     elif model== 'cosh':    beta, mu, alpha, ilam = 1, 'st', i, -1.5
     #elif model== 'pow2_exp2':beta, mu, alpha, ilam = -i, 2, 2, 0.75
 
@@ -133,7 +133,7 @@ if not(model=='exp'):
         elif model=='pow':      beta, mu, ilam = 0, -i, -0.75
         elif model=='exp_pow2': beta, mu, alpha, ilam = -i, 0, 2, -0.50
         elif model== 'pow_exp': beta, mu, alpha, ilam = -i, 1, 1, 0.75
-        elif model== 'exp_pow_a':beta, mu, alpha, ilam = 0.8, 0, i, 0.75
+        elif model== 'exp_pow_a':beta, mu, alpha, ilam = -0.8, 0, i, -0.75
         elif model== 'cosh':     beta, mu, alpha, ilam = 1, 'st', i, 1.5
         T= PhiCosmology(beta=beta, mu=mu, alpha=alpha, ilam= ilam, curv=0)
 