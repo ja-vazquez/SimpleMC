59e2f6d14fed2b162f738c2e51e8c619c3ba454c
Jose A <jvazquez@bnl.gov>
2020-06-15 23:51:29 -0500

    IBEG model

--- simplemc/models/IBEGCosmology.py
+++ simplemc/models/IBEGCosmology.py
@@ -1,28 +1,44 @@
-## This is CDM cosmology with w, wa and Ok
 
 
-import math as N
-from LCDMCosmology import *
+from simplemc.models.LCDMCosmology import LCDMCosmology
+from simplemc.cosmo.paramDefs import *
+
+#Observational constraints on the free parameters of an
+# interacting Bose-Einstein gas as a dark-energy model
+#https://arxiv.org/abs/1802.07232
 
 class IBEGCosmology(LCDMCosmology):
     def __init__(self, varyOk=False, varyOi0=True, varyxx=True):
-        ## two parameters: Om and h
+        """
+        Dark energy is modelled by a Bose-Einstein gas of particles
+        with an attractive interaction.
+        Parameters
+        ----------
+        varyOk
+        varyOi0
+        varyxx
 
-	self.varyOk=varyOk
-	self.varyOi0=varyOi0
-	self.varyxx=varyxx
+        Returns
+        -------
 
-        self.Ok=Ok_par.value   
-        self.Oi0= Oi0_par.value
-        self.xx= xx_par.value
+        """
+
+        self.varyOk  = varyOk
+        self.varyOi0 = varyOi0
+        self.varyxx  = varyxx
+
+        self.Ok  = Ok_par.value
+        self.Oi0 = Oi0_par.value
+        self.xx  = xx_par.value
         LCDMCosmology.__init__(self)
+
 
     ## my free parameters. We add Ok on top of LCDM ones (we inherit LCDM)
     def freeParameters(self):
         l=LCDMCosmology.freeParameters(self)
         if (self.varyOk): l.append(Ok_par)
-	if (self.varyOi0): l.append(Oi0_par)
-	if (self.varyxx): l.append(xx_par)
+        if (self.varyOi0): l.append(Oi0_par)
+        if (self.varyxx): l.append(xx_par)
         return l
 
 
@@ -42,11 +58,12 @@ class IBEGCosmology(LCDMCosmology):
                    return False
         return True
 
+
     ## this is relative hsquared as a function of a
     ## i.e. H(z)^2/H(z=0)^2
     def RHSquared_a(self,a):
         NuContrib=self.NuDensity.rho(a)/self.h**2
-	Oc0 = (1.0-self.Om-self.Ok - self.Oi0/(1 - 2*self.xx))*(2- 5.*self.xx)/2.
-	rhow = 2.*Oc0*a**(5*self.xx-5.)/(2- 5*self.xx) + self.Oi0*a**(6*self.xx-6.)/(1- 2*self.xx) 
+        Oc0 = (1.0-self.Om-self.Ok - self.Oi0/(1 - 2*self.xx))*(2- 5.*self.xx)/2.
+        rhow = 2.*Oc0*a**(5*self.xx-5.)/(2- 5*self.xx) + self.Oi0*a**(6*self.xx-6.)/(1- 2*self.xx)
         return (self.Ocb/a**3+self.Ok/a**2+self.Omrad/a**4+NuContrib+rhow)
 
