2de413cc705d7e5f94d6cbd2d6f9a28eb7ab3126
igomezv <neorelativista@gmail.com>
2020-06-28 04:54:07 -0500

    bambi working well

--- neuralTest.py
+++ neuralTest.py
@@ -1,3 +1,4 @@
+# TODO neural net in parallel with multiprocessing
 from simplemc.DriverMC import DriverMC
 #import tensorflow as tf
 
@@ -6,10 +7,12 @@ from simplemc.DriverMC import DriverMC
 # outputs = tf.keras.layers.Dense(1, activation=tf.nn.relu)(x)
 # model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-analyzer = DriverMC(analyzername="nested", model="LCDM", datasets="HD", chainsdir="chains")
+analyzer = DriverMC(analyzername="nested", model="owaCDM", datasets="HD+SN+BBAO+Planck", chainsdir="chains")
 
-analyzer.nestedRunner(neuralNetwork=True, nlivepoints=50)
-analyzer.postprocess()
+# analyzer.nestedRunner(nlivepoints=50)
+analyzer.nestedRunner(neuralNetwork=True, nlivepoints=50, proxy_tolerance=1.0, ntrain=50,
+                       it_to_start_net=50, updInt=50, epochs=300, numNeurons=200, split=0.8)
+analyzer.postprocess()  
 
 # analyzer = DriverMC(iniFile='baseConfig.ini')
 # analyzer.executer()
