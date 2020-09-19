0c7800d8ca53d946a4526a1f0320718026f39ebd
igomezv <neorelativista@gmail.com>
2020-09-19 15:56:10 -0500

    test neural

--- neuralTest.py
+++ neuralTest.py
@@ -1,4 +1,6 @@
 from simplemc.DriverMC import DriverMC
+import numpy as np
+np.random.seed(1234)
 #import tensorflow as tf
 
 # inputs = tf.keras.Input(shape=(3,))
@@ -6,11 +8,10 @@ from simplemc.DriverMC import DriverMC
 # outputs = tf.keras.layers.Dense(1, activation=tf.nn.relu)(x)      
 # model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-analyzer = DriverMC(analyzername="nested", model="LCDM", datasets="HD+SN+BBAO", chainsdir="chains")
+analyzer = DriverMC(analyzername="nested", model="LCDM", datasets="HD+SN+BBAO+Planck", chainsdir="chains")
 
-# analyzer.nestedRunner(nlivepoints=50)
-analyzer.nestedRunner(neuralNetwork=True, nlivepoints=50, proxy_tolerance=10.0, dlogz_start=0.5)
-# analyzer.nestedRunner(neuralNetwork=True, nlivepoints=50, proxy_tolerance=1.0, ntrain=50,
-#                       it_to_start_net=50, updInt=50, epochs=300, numNeurons=200, split=0.8, nproc=2)
-analyzer.postprocess()  
+# analyzer.nestedRunner(nlivepoints=500)
+analyzer.nestedRunner(neuralNetwork=True, nlivepoints=500, proxy_tolerance=0.3, dlogz_start=10,
+                      numNeurons=50, failure_tolerance=0.2, epochs=100)
+analyzer.postprocess()
 
