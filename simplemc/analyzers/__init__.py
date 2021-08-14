1cdead0edab20992fd476ef6046dafbf96b574ae
igomezv <neorelativista@gmail.com>
2021-08-14 01:24:11 -0500

    add EnsambleSampler to init of analyzers

--- simplemc/analyzers/__init__.py
+++ simplemc/analyzers/__init__.py
@@ -1,6 +1,7 @@
 from .MaxLikeAnalyzer import MaxLikeAnalyzer
 from .MCMCAnalyzer import MCMCAnalyzer
 from .dynesty import DynamicNestedSampler, NestedSampler
+from .emcee import EnsembleSampler
 from .SimpleGenetic import SimpleGenetic
 from .GA_deap import GA_deap
 from .neuralike import NeuralManager
