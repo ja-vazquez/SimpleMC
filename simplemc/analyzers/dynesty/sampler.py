7d068b5cbe87d6006af3ed2075639f7b77dc50b5
igomezv <neorelativista@gmail.com>
2021-08-16 04:50:09 -0500

    change name of output file with samples

--- simplemc/analyzers/dynesty/sampler.py
+++ simplemc/analyzers/dynesty/sampler.py
@@ -997,7 +997,7 @@ class Sampler(object):
                 f.write("{}\n".format(rowstr))
                 f.flush()
         f.close()
-        f = open(self.outputname + '_1.txt', "+a")
+        f = open(self.outputname + '.txt', "+a")
         # Add remaining live points to samples.
         if add_live:
             for i, results in enumerate(self.add_live_points()):
