561be17f3c12678593e9740449e7e065a1694054
seVenVo1d <arman-cam@windowslive.com>
2021-09-01 20:17:17 +0300

    creating a GUI for LCDM model and mcmc sampler

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from simplemc.DriverMC import DriverMC
-from os import remove
 
 "read all setting from .ini file"
 inifile = "baseConfigGUI.ini"
@@ -7,9 +6,6 @@ inifile = "baseConfigGUI.ini"
 analyzer = DriverMC(iniFile=inifile)
 analyzer.executer()
 #analyzer.postprocess()
-
-remove("baseConfigGUI.ini")
-
 
 """ useful for short tests,
     or when just a few settings customize it from here"""
