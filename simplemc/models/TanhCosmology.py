fd99859b4f9a23f4c4253dae211b830530a1a327
ja-vazquez <javazquez@icf.unam.mx>
2022-01-17 17:44:46 -0600

    testing reconstruction

--- simplemc/models/TanhCosmology.py
+++ simplemc/models/TanhCosmology.py
@@ -6,29 +6,29 @@ from scipy.integrate import quad
 from scipy.interpolate import interp1d
 from simplemc.cosmo.Parameter import Parameter
 
-
+import matplotlib.pyplot as plt
 
 class TanhCosmology(LCDMCosmology):
     def __init__(self):
 
 
-        Nbins_eos = 4
-        step_eos = -1
-        zbin_eos_par = [Parameter("zbin_eos%d"%i, step_eos, 0.2, (-3.5, 0), "zbin_eos%d"%i) for i in range(Nbins_eos)]
-        self.parvals = zbin_eos_par
-        self.zs = [i.value for i in self.parvals]
-        names   = [i.name  for i in self.parvals]
-        self.index = dict((j,i) for i,j in enumerate(names))
+        self.Nbins_eos = 3
+        mean_eos = -1
+        self.params = [Parameter("zbin_eos%d"%i, mean_eos, 0.2, (-3.5, 0), "zbin_eos%d"%i) for i in range(self.Nbins_eos)]
+        self.pvals = [i.value for i in self.params]
+        self.z_i = np.linspace(0.0, 3.0, self.Nbins_eos+1)
 
-        #self.zvals = np.logspace(np.log10(0.01),np.log10(2.261), len(self.parvals)+1)
+        self.zinter = np.linspace(0.0, 3.0, 50)
 
-        LCDMCosmology.__init__(self)
+        LCDMCosmology.__init__(self, mnu=0)
+        self.updateParams([])
+
 
     # my free parameters. We add Ok on top of LCDM ones (we inherit LCDM)
     def freeParameters(self):
         l = LCDMCosmology.freeParameters(self)
         #l.append(zbin_rho_par)
-        l+= self.parvals
+        l+= self.params
         return l
 
     def updateParams(self, pars):
@@ -36,20 +36,52 @@ class TanhCosmology(LCDMCosmology):
         if not ok:
             return False
         for p in pars:
-            for i in range(len(self.parvals)):
+            for i in range(self.Nbins_eos):
                 if p.name == ("zbin_eos"+str(i)):
-                    self.zs[i] = p.value
+                    self.pvals[i] = p.value
+
+        self.initialize()
         return True
+
+     
+    def bines(self, w_2, w_1, z_2, z_1, eta):
+        return (w_2-w_1)*(1.0+np.tanh((z_2-z_1)/eta))/2.0
+
+
+    def de_eos(self, z):
+        w = self.pvals[0]
+        for jj in range(self.Nbins_eos - 1):
+            w+=self.bines(self.pvals[jj+1], self.pvals[jj], z, self.z_i[jj+1], eta=0.15)
+        return w
+
+
+    def de_rhow(self, z):
+        eos = self.de_eos(z)
+        resultado = quad(lambda b: 3.0*(1.0+ eos)/(1.0+b), 0.0, z )
+        return resultado[0]
+   
+
+    def initialize(self):
+        #w_inter = [self.de_eos(z) for z in self.zinter]
+        rhow = [self.de_rhow(z) for z in self.zinter]
+        self.rhow_inter = interp1d(self.zinter, rhow)
+
+        #plt.plot(self.zinter, rhow)
+        #plt.show()
+        return True
+
+    
+
 
     def luisfunction(self, z):
         def luisfunction2(z):
-            w_i = self.zs
-            z_i = np.linspace(0.0,3.0,len(self.parvals)+1)
-            def bines(w_2,w_1,z_2,z_1,eta):
-                return (w_2-w_1)*(1.0+np.tanh((z_2-z_1)/eta))/2.0
-            w = self.zs[0]
-            for jj in range(len(self.zs)-1):
-                w+=bines(self.zs[jj+1],self.zs[jj],z,z_i[jj+1],eta=0.15)
+            #w_i = self.pvals
+            #z_i = np.linspace(0.0,3.0, self.Nbins_eos+1)
+            #def bines(w_2,w_1,z_2,z_1,eta):
+            #    return (w_2-w_1)*(1.0+np.tanh((z_2-z_1)/eta))/2.0
+            w = self.pvals[0]
+            for jj in range(self.Nbins_eos - 1):
+                w+=self.bines(self.pvals[jj+1],self.pvals[jj], z, self.z_i[jj+1],eta=0.15)
             rhow=w
             return rhow
         rhow=luisfunction2(z)
@@ -60,8 +92,11 @@ class TanhCosmology(LCDMCosmology):
     ## this is relative hsquared as a function of a
     ## i.e. H(z)^2/H(z=0)^2
     def RHSquared_a(self,a):
-        NuContrib=self.NuDensity.rho(a)/self.h**2
         z= 1./a - 1
-        return (self.Ocb/a**3+self.Omrad/a**4+NuContrib+(1.0-self.Om)*(np.exp(self.luisfunction(z)[1]))  )
-
+        if z>= 3.0:
+            rhow = (1.0-self.Om)
+        else:
+            rhow = (1.0-self.Om)*np.exp(self.rhow_inter(z)) 
+        #return (self.Ocb/a**3+self.Omrad/a**4 +(1.0-self.Om)*(np.exp(self.luisfunction(z)[1]))  )
+        return self.Ocb/a**3 + self.Omrad/a**4 + rhow
 
