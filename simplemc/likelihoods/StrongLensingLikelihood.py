d7723b648966d7272a80e3e9cfd80c1db9498372
ja-vazquez <javazquez@icf.unam.mx>
2024-02-02 14:18:45 -0600

    increasing the number of strong lensing values

--- simplemc/likelihoods/StrongLensingLikelihood.py
+++ simplemc/likelihoods/StrongLensingLikelihood.py
@@ -27,7 +27,8 @@ class StrongLensingLikelihood(BaseLikelihood):
         self.zl = da[:,0]
         self.zs = da[:, 1]
         self.theta_E = da[:,2]
-        self.sigma = da[:,3]
+        self.sigma = da[:,4]
+        
         print("Loading ", cov_filename)
         cov = sp.loadtxt(cov_filename, skiprows=1)
         assert(len(cov) == len(self.zs))
@@ -49,12 +50,12 @@ class StrongLensingLikelihood(BaseLikelihood):
 
         tvec += 0
         delta = tvec - tobs
-        return -sp.dot(delta, sp.dot(self.icov, delta))/2.0
-
+        loglike= -sp.dot(delta, sp.dot(self.icov, delta))/2.0
+        return loglike
 
 class StrongLensing(StrongLensingLikelihood):
     # data from https://arxiv.org/abs/1906.04107
     def __init__(self):
-        StrongLensingLikelihood.__init__(self, "SL", cdir+"/data/Strong_Lensing.txt",
-                                            cdir+"/data/Strong_Lensing_cov.txt")
+        StrongLensingLikelihood.__init__(self, "SL", cdir+"/data/Strong_Lensing_196.txt",
+                                            cdir+"/data/Strong_Lensing_cov_196.txt")
 
