f219854ba6931281b67ba9edb9610410fe707b18
ja-vazquez <javazquez@icf.unam.mx>
2025-03-08 12:17:00 -0600

    reconstructing HDE

--- simplemc/models/HolographicCosmology.py
+++ simplemc/models/HolographicCosmology.py
@@ -1,11 +1,12 @@
 from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.interpolate import UnivariateSpline
 from simplemc.models.LCDMCosmology import LCDMCosmology
 from simplemc.cosmo.Parameter import Parameter
 from simplemc.cosmo.paramDefs import Ok_par
 from scipy.interpolate import interp1d
 from scipy.integrate import odeint
 import numpy as np
-
+from scipy import constants
 
 
 class HolographicCosmology(LCDMCosmology):
@@ -17,13 +18,17 @@ class HolographicCosmology(LCDMCosmology):
         :param varyc: variable w0 parameter
 
     """
-    def __init__(self, varyOk=False, varychde=True,
-                 nodes=3  # numer of nodes used in the interpolation
+    def __init__(self, varyOk=False, varychde=True, mean=-1,
+                 nodes=3,   # numer of nodes used in the interpolation
+                 interp= 'lineal'
                  ):
         # Holographic parameter
         # Notation: capital C = 3c**2*M_p, here, we use little c
+        # Nodes: =0 delta constant, = 1 varying delta, = 2 two nodes, etc.
 
         self.nnodes = nodes
+        # =1 lineal, 3 = cubic
+        self.interp = 1 if interp == 'lineal' else 3
 
         self.varyOk = varyOk
         self.varychde = varychde
@@ -36,18 +41,19 @@ class HolographicCosmology(LCDMCosmology):
         # zend is the last point in linear-interpolation
         # however is the first point of the last step in the binning/tanh version
         self.zini = 0.0
-        self.zend = 3.0
+        self.zend = 2.5
 
         # range to perform the interpolation
         self.zvals = np.linspace(self.zini, self.zend, 100)
 
-
-        mean = -2
-        priors = (-1, 3)
+        # = 0 for CC, = -2 for Barrow
+        mean = mean
+        priors = (-3, 1)
         sigma = 0.2
 
         self.pname = 'amp_'
-        self.params = [Parameter(self.pname+str(i), mean, sigma, priors, self.pname+str(i)) for i in range(nodes)]
+        amps = 1 if nodes <= 1 else nodes
+        self.params = [Parameter(self.pname+str(i), mean, sigma, priors, self.pname+str(i)) for i in range(amps)]
         self.pvals = [i.value for i in self.params]
 
         LCDMCosmology.__init__(self, disable_radiation=True)
@@ -57,9 +63,10 @@ class HolographicCosmology(LCDMCosmology):
     # my free parameters. We add Ok on top of LCDM ones (we inherit LCDM)
     def freeParameters(self):
         l = LCDMCosmology.freeParameters(self)
-        l += self.params
         if (self.varychde):  l.append(self.c_par)
         if (self.varyOk): l.append(Ok_par)
+        if self.nnodes > 0:
+            l += self.params
         return l
 
 
@@ -68,9 +75,11 @@ class HolographicCosmology(LCDMCosmology):
         if not ok:
             return False
 
-        for i, p in enumerate(pars):
-            if p.name == (self.pname+str(i)):
-                self.pvals[i] = p.value
+        for p in pars:
+            if self.pname in p.name:
+                for i in range(self.nnodes):
+                    if p.name == (self.pname+str(i)):
+                        self.pvals[i] = p.value
             if p.name == "c":
                 self.c_hde = p.value
             elif p.name == "Ok":
@@ -79,68 +88,54 @@ class HolographicCosmology(LCDMCosmology):
                 if (abs(self.Ok) > 1.0):
                     return False
 
-        if self.nnodes > 1:
-            self.ini_function()
+        self.ini_function()
         self.initialize()
         return True
 
 
 
-
     def ini_function(self):
-        self.y = self.pvals
-        self.mnodes = self.nnodes-1
+        if self.nnodes <=1:
+            amp = 1
+            y = self.pvals[0]*np.ones(2)
+        else:
+            amp = self.nnodes-1
+            y = self.pvals
 
-        delta = (self.zend - self.zini)/(self.mnodes)
-        self.x = [self.zini + delta*i for i in range(self.mnodes+1)]
+        delta = (self.zend - self.zini)/amp
+        x = [self.zini + delta*i for i in range(amp+1)]
 
-        self.spline = InterpolatedUnivariateSpline(self.x, self.y, k=1)
-
+        self.ffunc = InterpolatedUnivariateSpline(x, y, k=self.interp)
+        self.dffunc = self.ffunc.derivative(n=1)
         return True
 
+    #def dffunc(self, z):
 
-
-    def ffunc(self, z):
-        # = 0 for CC, = 2 for Barrow
-        # f = \Delta-2
-        # function = self.alpha_hde + self.beta_hde*z
-        if self.nnodes==1:
-            funct = self.pvals[0]
-        else:
-            funct = self.spline(z)
-        return funct
-
-
-    def dffunc(self, z):
-        # dfunction = self.beta_hde
-        if self.nnodes==1:
-            dfunc = 0
-        else:
-            dfucn = x
-        return dfunc
 
 
     def Q_term(self, z):
         delta = self.ffunc(z) + 2
-        term1 = 1./(delta - 2)
+        term1 = 1./self.ffunc(z)
 
-        Q = (2-delta)*(self.c_hde)**(2*term1)*(100*self.h*np.sqrt(self.Om))**(-delta*term1)
+        h0_c= 100*self.h/(constants.c/1000.)
+        Q = (2-delta)*(self.c_hde)**(2*term1)*(h0_c*np.sqrt(self.Om))**(-delta*term1)
+
         return Q
 
 
     def extra_term(self, z, Ode):
         delta = self.ffunc(z) + 2
-        term1 = 1./(delta - 2)
+        term1 = 1./self.ffunc(z)
 
         return (1 - Ode)**(0.5*delta*term1)*Ode**(-term1)
 
 
     def deriv_func(self, z, Ode):
         delta = self.ffunc(z) + 2
-        term1 = 1. / (delta - 2)
+        term1 = 1./self.ffunc(z)
 
         qterm= self.c_hde**2*(1+z)**(-3)/(100*self.h)**2/self.Om
-        # this is where the sign changes compared to previous works
+        # below is where the sign changes compared to previous works
         value= (1 + z)*self.dffunc(z)*term1*np.log(qterm*(1 - Ode)/Ode)
         return value
 
@@ -148,10 +143,13 @@ class HolographicCosmology(LCDMCosmology):
     # Right hand side of the equations
     def RHS_hde(self, Ode, z):
         delta = self.ffunc(z) + 2
-        term1 = 1./(delta - 2)
+        term1 = 1./self.ffunc(z)
 
-        term_cte = 1 + delta + self.Q_term(z)*self.extra_term(z, Ode)*(1 + z)**(-1.5*delta*term1)
-        fact = term_cte + self.deriv_func(z, Ode)
+        if np.abs(self.ffunc(z)) <= 0.005 : #np.allclose(self.ffunc(z), 0):
+            fact = 1 + delta
+        else:
+            term_cte = 1 + delta + self.Q_term(z)*self.extra_term(z, Ode)*(1 + z)**(-1.5*delta*term1)
+            fact = term_cte + self.deriv_func(z, Ode)
         dOmega = -Ode*(1 - Ode)*fact/(1 + z)
         return dOmega
 
@@ -173,7 +171,7 @@ class HolographicCosmology(LCDMCosmology):
     ## i.e. H(z)^2/H(z=0)^2
     def RHSquared_a(self, a):
         z = 1./a-1
-        if z>self.zini:
+        if z>self.zend:
             Ode =  (1-self.Om-self.Ok)
             hubble = self.Omrad/a**4 + self.Ocb/a**3 + self.Ok/a**2 + Ode
         else:
@@ -184,9 +182,12 @@ class HolographicCosmology(LCDMCosmology):
 
     def EoS(self, z):
         Ode = self.Ode(z)
-        delta = self.ffunc(z)
+        delta = self.ffunc(z) + 2
 
-        tmp = (1+z)**(-1.5*delta/(2-delta))
-        w = -(1+delta) - self.Q_term(z)*self.extra_term(z, Ode)*tmp \
+        if np.abs(self.ffunc(z)) <= 0.005: #np.allclose(self.ffunc(z), 0):
+            fact = -(1 + delta)
+        else:
+            tmp = (1+z)**(-1.5*delta/(2-delta))
+            fact = -(1+delta) - self.Q_term(z)*self.extra_term(z, Ode)*tmp \
             + self.deriv_func(z, Ode)
-        return w/3.
+        return fact/3.
